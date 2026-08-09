ee7d43f65382985e18a1dd4ceb8d83788fed7918
Luna <luna.wenczel@stud.hs-emden-leer.de>
2026-08-09 13:20:00 +0200

    added suppitem

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 //randomizing functionality
 function randomizeAll() {
+    
     randomizeJunglePet();
+    randomizeSuppItem();
 }
 
 function randomizeJunglePet() {
-    let junglePetNr = Math.floor(Math.random() * 3);
-    let junglePet;
+    let junglePetList = document.getElementById("jungleStarterPets").children;
+    let junglePetAmount = junglePetList.length;
+    let junglePetNr = Math.floor(Math.random() * junglePetAmount);
+    let junglePet = junglePetList[junglePetNr];
     console.log(junglePetNr);
-    if (junglePetNr == 0) {
-        junglePet = document.getElementById("greenPet");
-    } else if (junglePetNr == 1) {
-        junglePet = document.getElementById("redPet");
-    } else {
-        junglePet = document.getElementById("bluePet");
-    } 
+    console.log(junglePet);
     displayJunglePet(junglePet);
+}
+
+function randomizeSuppItem() {
+    let supportItemsList = document.getElementById("supportItems").children;
+    let suppItemAmount = supportItemsList.length;
+    let suppItemNr = Math.floor(Math.random() * suppItemAmount);
+    let suppItem = supportItemsList[suppItemNr];
+    displaySuppItem(suppItem);
 }
 
 
@@ -27,6 +33,15 @@ function displayJunglePet(junglePet) {
     junglePetImg.setAttribute("alt", junglePet.alt);
     if (junglePetCont.children.length < 1) {
         junglePetCont.appendChild(junglePetImg);
-    }
-    
+    }   
+}
+
+function displaySuppItem(suppItem) {
+    let suppItemCont = document.getElementById("suppItem");
+    let suppItemImg = document.createElement("IMG");
+    suppItemImg.setAttribute("src", suppItem.src);
+    suppItemImg.setAttribute("alt", suppItem.alt);
+    if (suppItemCont.children.length < 1) {
+        suppItemCont.appendChild(suppItemImg);
+    }   
 }
